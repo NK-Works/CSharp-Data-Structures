7ce29a63b068fb2ea567b8ce94af7efafbca67fb
NK-Works <132702983+NK-Works@users.noreply.github.com>
2023-11-05 20:09:04 +0530

    Add files via upload

---
+++ Task 5.1P/DoublyLinkedList.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+    public class DoublyLinkedList<T>
+    {
+
+        // Here is the the nested Node<K> class 
+        private class Node<K> : INode<K>
+        {
+            public K Value { get; set; }
+            public Node<K> Next { get; set; }
+            public Node<K> Previous { get; set; }
+
+            public Node(K value, Node<K> previous, Node<K> next)
+            {
+                Value = value;
+                Previous = previous;
+                Next = next;
+            }
+
+            // This is a ToString() method for the Node<K>
+            // It represents a node as a tuple {'the previous node's value'-(the node's value)-'the next node's value')}. 
+            // 'XXX' is used when the current node matches the First or the Last of the DoublyLinkedList<T>
+            public override string ToString()
+            {
+                StringBuilder s = new StringBuilder();
+                s.Append("{");
+                s.Append(Previous.Previous == null ? "XXX" : Previous.Value.ToString());
+                s.Append("-(");
+                s.Append(Value);
+                s.Append(")-");
+                s.Append(Next.Next == null ? "XXX" : Next.Value.ToString());
+                s.Append("}");
+                return s.ToString();
+            }
+
+        }
+
+        // Here is where the description of the methods and attributes of the DoublyLinkedList<T> class starts
+
+        // An important aspect of the DoublyLinkedList<T> is the use of two auxiliary nodes: the Head and the Tail. 
+        // The both are introduced in order to significantly simplify the implementation of the class and make insertion functionality reduced just to a AddBetween(...)
+        // These properties are private, thus are invisible to a user of the data structure, but are always maintained in it, even when the DoublyLinkedList<T> is formally empty. 
+        // Remember about this crucial fact when you design and code other functions of the DoublyLinkedList<T> in this task.
+        private Node<T> Head { get; set; }
+        private Node<T> Tail { get; set; }
+        public int Count { get; private set; } = 0;
+
+        public DoublyLinkedList()
+        {
+            Head = new Node<T>(default(T), null, null);
+            Tail = new Node<T>(default(T), Head, null);
+            Head.Next = Tail;
+        }
+
+        public INode<T> First
+        {
+            get
+            {
+                if (Count == 0) return null;
+                else return Head.Next;
+            }
+        }
+
+        public INode<T> Last
+        {
+            get
+            {
+                if (Count == 0) return null;
+                else return Tail.Previous;
+            }
+        }
+
+        public INode<T> After(INode<T> node)
+        {
+            if (node == null) throw new NullReferenceException();
+            Node<T> node_current = node as Node<T>;
+            if (node_current.Previous == null || node_current.Next == null) throw new InvalidOperationException("The node referred as 'before' is no longer in the list");
+            if (node_current.Next.Equals(Tail)) return null;
+            else return node_current.Next;
+        }
+
+        public INode<T> AddLast(T value)
+        {
+            return AddBetween(value, Tail.Previous, Tail);
+        }
+
+        // This is a private method that creates a new node and inserts it in between the two given nodes referred as the previous and the next.
+        // Use it when you wish to insert a new value (node) into the DoublyLinkedList<T>
+        private Node<T> AddBetween(T value, Node<T> previous, Node<T> next)
+        {
+            Node<T> node = new Node<T>(value, previous, next);
+            previous.Next = node;
+            next.Previous = node;
+            Count++;
+            return node;
+        }
+
+        public INode<T> Find(T value)
+        {
+            Node<T> node = Head.Next;
+            while (!node.Equals(Tail))
+            {
+                if (node.Value.Equals(value)) return node;
+                node = node.Next;
+            }
+            return null;
+        }
+
+        public override string ToString()
+        {
+            if (Count == 0) return "[]";
+            StringBuilder s = new StringBuilder();
+            s.Append("[");
+            int k = 0;
+            Node<T> node = Head.Next;
+            while (!node.Equals(Tail))
+            {
+                s.Append(node.ToString());
+                node = node.Next;
+                if (k < Count - 1) s.Append(",");
+                k++;
+            }
+            s.Append("]");
+            return s.ToString();
+        }
+
+        // TODO: Your task is to implement all the remaining methods.
+        // Read the instruction carefully, study the code examples from above as they should help you to write the rest of the code.
+
+        public INode<T> Before(INode<T> node)
+        {
+            if (node == null) throw new NullReferenceException();
+            
+            Node<T> node_current = node as Node<T>;
+            if (node_current.Previous == null || node_current.Next == null) throw new InvalidOperationException("The node referred as 'after' is no longer in the list");
+            
+            if (node_current.Previous.Equals(Head)) return null;
+            else return node_current.Previous;
+        }
+
+        public INode<T> AddFirst(T value)
+        {
+            return AddBetween(value, Head, Head.Next);
+        }
+
+        public INode<T> AddBefore(INode<T> before, T value)
+        {
+            if (before == null) throw new NullReferenceException();
+            
+            Node<T> node_current = before as Node<T>;
+            return AddBetween(value, node_current.Previous, node_current);
+        }
+
+        public INode<T> AddAfter(INode<T> after, T value)
+        {
+            if (after == null) throw new NullReferenceException();
+            
+            Node<T> node_current = after as Node<T>;
+            return AddBetween(value, node_current, node_current.Next);
+        }
+
+        public void Clear()
+        {
+            Head.Next = Tail;
+            Tail.Previous = Head;
+            Count = 0;
+        }
+
+        public void Remove(INode<T> node)
+        {
+            if (Count == 0) throw new InvalidOperationException();
+            if (node == null) throw new NullReferenceException();
+            if (Find(node.Value) == null) throw new InvalidOperationException();
+            
+            Node<T> node_current = node as Node<T>;
+            node_current.Next.Previous = node_current.Previous;
+            node_current.Previous.Next = node_current.Next;
+            
+            node_current.Next = null;
+            node_current.Previous = null;
+            Count--;
+        }
+
+        public void RemoveFirst()
+        {
+            Remove(First);
+        }
+
+        public void RemoveLast()
+        {
+            Remove(Last);
+        }
+    }
+}
+
+/*-----------------------------------------------My Output-------------------------------------------------------
+Test A: Create a new list by calling 'DoublyLinkedList<int> vector = new DoublyLinkedList<int>( );'
+ :: SUCCESS: list's state []
+
+Test B: Add a sequence of numbers 2, 6, 8, 5, 1, 8, 5, 3, 5 with list.AddLast( )
+ :: SUCCESS: list's state [{XXX-(2)-6},{2-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-8},{1-(8)-5},{8-(5)-3},{5-(3)-5},{3-(5)-XXX}]
+
+Test C: Remove sequentially 4 last numbers with list.RemoveLast( )
+ :: SUCCESS: list's state [{XXX-(2)-6},{2-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+
+Test D: Add a sequence of numbers 10, 20, 30, 40, 50 with list.AddFirst( )
+ :: SUCCESS: list's state [{XXX-(50)-40},{50-(40)-30},{40-(30)-20},{30-(20)-10},{20-(10)-2},{10-(2)-6},{2-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+
+Test E: Remove sequentially 3 last numbers with list.RemoveFirst( )
+ :: SUCCESS: list's state [{XXX-(20)-10},{20-(10)-2},{10-(2)-6},{2-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+
+Test F: Run a sequence of operations:
+list.Find(40);
+ :: SUCCESS: list's state [{XXX-(20)-10},{20-(10)-2},{10-(2)-6},{2-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+list.Find(0);
+ :: SUCCESS: list's state [{XXX-(20)-10},{20-(10)-2},{10-(2)-6},{2-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+list.Find(2);
+ :: SUCCESS: list's state [{XXX-(20)-10},{20-(10)-2},{10-(2)-6},{2-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+
+Test G: Run a sequence of operations:
+Add 100 before the node with 2 with list.AddBefore(2,100)
+ :: SUCCESS: list's state [{XXX-(20)-10},{20-(10)-100},{10-(100)-2},{100-(2)-6},{2-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+Add 200 after the node with 2 with list.AddAfter(2,200)
+ :: SUCCESS: list's state [{XXX-(20)-10},{20-(10)-100},{10-(100)-2},{100-(2)-200},{2-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+Add 300 before node list.First with list.AddBefore(list.First,300)
+ :: SUCCESS: list's state [{XXX-(300)-20},{300-(20)-10},{20-(10)-100},{10-(100)-2},{100-(2)-200},{2-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+Add 400 after node list.First with list.AddAfter(list.First,400)
+ :: SUCCESS: list's state [{XXX-(300)-400},{300-(400)-20},{400-(20)-10},{20-(10)-100},{10-(100)-2},{100-(2)-200},{2-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-1},{5-(1)-XXX}]
+Add 500 before node list.First with list.AddBefore(list.Last,500)
+ :: SUCCESS: list's state [{XXX-(300)-400},{300-(400)-20},{400-(20)-10},{20-(10)-100},{10-(100)-2},{100-(2)-200},{2-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-500},{5-(500)-1},{500-(1)-XXX}]
+Add 600 after node list.First with list.AddAfter(list.Last,600)
+ :: SUCCESS: list's state [{XXX-(300)-400},{300-(400)-20},{400-(20)-10},{20-(10)-100},{10-(100)-2},{100-(2)-200},{2-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-500},{5-(500)-1},{500-(1)-600},{1-(600)-XXX}]
+
+Test H: Run a sequence of operations:
+Remove the node list.First with list.Remove(list.First)
+ :: SUCCESS: list's state [{XXX-(400)-20},{400-(20)-10},{20-(10)-100},{10-(100)-2},{100-(2)-200},{2-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-500},{5-(500)-1},{500-(1)-600},{1-(600)-XXX}]
+Remove the node list.Last with list.Remove(list.Last)
+ :: SUCCESS: list's state [{XXX-(400)-20},{400-(20)-10},{20-(10)-100},{10-(100)-2},{100-(2)-200},{2-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-500},{5-(500)-1},{500-(1)-XXX}]
+Remove the node list.Before, which is before the node containing element 2, with list.Remove(list.Before(...))
+ :: SUCCESS: list's state [{XXX-(400)-20},{400-(20)-10},{20-(10)-2},{10-(2)-200},{2-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-500},{5-(500)-1},{500-(1)-XXX}]
+Remove the node containing element 2 with list.Remove(...)
+ :: SUCCESS: list's state [{XXX-(400)-20},{400-(20)-10},{20-(10)-200},{10-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-500},{5-(500)-1},{500-(1)-XXX}]
+
+Test I: Remove the node containing element 2, which has been recently deleted, with list.Remove(...)
+ :: SUCCESS: list's state [{XXX-(400)-20},{400-(20)-10},{20-(10)-200},{10-(200)-6},{200-(6)-8},{6-(8)-5},{8-(5)-500},{5-(500)-1},{500-(1)-XXX}]
+
+Test J: Clear the content of the vector via calling vector.Clear();
+ :: SUCCESS: list's state []
+
+Test K: Remove last element for the empty list with list.RemoveLast()
+ :: SUCCESS: list's state []
+
+
+ ------------------- SUMMARY -------------------
+
+| This Code is made by Anneshu Nag, Student ID- 2210994760 |
+Tests passed: ABCDEFGHIJK */
---
+++ Task 6.2D/BoxOfCoins.cs
@@ -0,0 +1,168 @@
+﻿/* This code is made by Anneshu Nag, Student ID- 2210994760  */
+/*                    Dated- 27/09/2023                      */
+
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BoxOfCoins
+{
+    public class BoxOfCoins
+    {
+       public static int Solve(int[] boxes)
+        {
+             // Finding the total sum of all box values
+            int totalSum = 0; 
+            for(int i = 0; i <= boxes.Length - 1; i++)
+            {
+                totalSum += boxes[i];
+            }  
+
+            // Finding the sum that Alex will get
+            int sumAlex = AlexSum(boxes, boxes.Length); 
+            
+            // Finding Cindy's sum using Alex's
+            int sumCindy = totalSum - sumAlex; 
+
+            // Resultant sum i.e., Alex Sum - Cindy Sum
+            int resultSum = sumAlex - sumCindy;  
+            return resultSum;
+        }
+
+        // Private method needed to find Alex's sum
+        private static int AlexSum(int[] boxes, int noOfBoxes) 
+        {
+            // Table to store the maximum sum for Alex in different scenarios
+            int[,] tableSum = new int[noOfBoxes, noOfBoxes]; 
+
+            for(int i = 1; i <= noOfBoxes; i++)   // To loop over the subarrays
+            {
+                for(int startIdx = 0; startIdx <= noOfBoxes - i; startIdx++)
+                {
+                    int endIdx = i - 1 + startIdx; // Represents the end index
+                    int sumAtLeftBox, sumAfterSkippingOneBox, sumAfterSkippingTwoBox;
+                    
+                    // Maximum sum if Alex chooses the left box (opponent will get)
+                    if (startIdx + 1 < noOfBoxes && endIdx - 1 >= 0) 
+                        sumAtLeftBox = tableSum[startIdx + 1, endIdx - 1];
+
+                    else
+                        sumAtLeftBox = 0;
+                    
+                    // Maximum sum if Alex chooses the right box, skipping one box on the left 
+                    if (startIdx + 2 < noOfBoxes) 
+                        sumAfterSkippingOneBox = tableSum[startIdx + 2, endIdx];
+
+                    else
+                        sumAfterSkippingOneBox = 0;
+
+                    // Maximum sum if Alex chooses the right box, skipping two boxes on the left (before endIdx i.e. 2 left of last)
+                    if (endIdx - 2 >= 0) 
+                        sumAfterSkippingTwoBox = tableSum[startIdx,endIdx-2];
+
+                    else
+                        sumAfterSkippingTwoBox = 0;
+
+                    // The maximum sum Alex can achieve
+                    tableSum[startIdx, endIdx] = Math.Max(boxes[startIdx] + Math.Min(sumAtLeftBox, sumAfterSkippingOneBox) , boxes[endIdx] + Math.Min(sumAtLeftBox, sumAfterSkippingTwoBox)); 
+                }
+            }
+            // Maximum sum that Alex can achieve when all boxes are in tableSum[0, noOfBoxes-1]
+            return tableSum[0, noOfBoxes - 1]; 
+        }
+    }
+}
+
+/* ------------------------------------------------------------My Output-----------------------------------------------------------------
+Attempting test instance 0 with [7, 2] as the argument and 5 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 1 with [2, 7, 3] as the argument and -2 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 2 with [1000, 1000, 1000, 1000, 1000] as the argument and 1000 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 3 with [823, 912, 345, 100000, 867, 222, 991, 3, 40000] as the argument and -58111 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 4 with [23, 35, 12, 100000, 99234, 86123, 3245] as the argument and -83644 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 5 with [23, 35, 12, 100000, 99234, 86123, 3245, 1] as the argument and 83645 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 6 with [7, 7, 7, 7, 7, 7, 80, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7] as the argument and -66 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 7 with [7, 7, 7, 7, 7, 7, 7, 80, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7] as the argument and 73 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 8 with [91, 56, 23, 45, 87, 65, 45, 45, 78, 23, 20, 41, 17, 54, 51, 51, 94, 62, 74, 42, 76, 76] as the argument and 96 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 9 with [92834, 95461, 15911, 56189, 6369, 80545, 31811, 51263, 30076, 68867, 36905, 32499, 59799, 334, 82991, 46636, 98741, 66601] as the argument and 42958 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 10 with [129] as the argument and 129 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 11 with [35463, 88121, 4362, 94457, 86235, 83680, 72686, 6003, 93069, 2015, 10436, 2139, 93162, 30380, 19067, 76335, 78941, 48620, 55887, 15679] as the argument and 101879 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 12 with [19335, 97643, 11468, 86267, 79718, 59584, 12129, 52642, 86575, 62307, 11545, 52658, 72377, 39986, 74850, 1992, 86928] as the argument and 1846 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 13 with [91883, 97793, 54567, 64714, 98624] as the argument and 82567 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 14 with [98473, 41866, 71129, 65936, 42626, 9194, 46718, 96921, 45613, 47677, 8763, 54634, 47259, 71448, 9918, 22666, 32711, 21692, 40207, 2017, 23040, 86083, 77809, 15472, 30718, 39085, 87911, 54827, 41686, 28354, 37203, 6548, 74184, 3043, 43961, 95189, 1238, 22002, 93507, 63546, 32527, 42778, 31614] as the argument and -14953 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 15 with [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50] as the argument and 25 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 16 with [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] as the argument and 0 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 17 with [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 11, 11, 11, 11, 111, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 112, 312, 312, 123, 123, 123, 123, 123, 123, 123, 123, 123, 123, 123, 123, 123, 123, 231, 31, 312] as the argument and 316 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 18 with [1234, 1233, 12, 312, 32, 23, 434, 12, 312, 45, 1234, 1233, 12, 312, 32, 23, 434, 12, 312, 45, 1234, 1233, 12, 312, 32, 23, 434, 12, 312, 45, 1234, 1233, 12, 312, 32, 23, 434, 12, 312, 45, 1234, 1233, 12, 312, 32, 23, 434, 12, 312, 45] as the argument and 1995 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 19 with [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3] as the argument and 1 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 20 with [9, 100, 1, 8] as the argument and 98 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 21 with [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 66, 5, 4, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 6, 5, 4, 5, 6, 3, 4, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6] as the argument and 68 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 22 with [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 1, 2, 3, 4, 65, 67, 2, 3, 4, 7, 2, 3, 4, 6, 6, 7, 2, 3, 4, 7, 78, 8, 82, 2, 3, 4, 7, 2, 2, 34, 4, 6, 7, 3, 2] as the argument and 128 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 23 with [100, 10, 10] as the argument and 100 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 24 with [1] as the argument and 1 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 25 with [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 3, 5, 4, 6, 7, 5, 6, 10, 2, 5, 4, 3, 4, 5, 6, 7, 9, 10] as the argument and 28 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 26 with [6, 4, 3, 5, 8, 8] as the argument and 2 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 27 with [1, 5, 20, 2, 1] as the argument and -13 as the expected answer
+ :: SUCCESS
+
+Attempting test instance 28 with [1, 2, 3, 4, 5, 6, 6, 7, 8, 767, 765, 111, 76576, 5, 64, 654, 64, 7, 7657, 76575, 64, 65, 6454, 64, 654, 65464, 7, 5435, 65, 746, 7, 546, 7, 654, 7, 5435, 547, 6, 6, 7, 6547, 7654, 6, 754, 54353, 65, 7, 8] as the argument and 118231 as the expected answer
+ :: SUCCESS
+
+Summary: 29 tests out of 29 passed
+Tests passed (0 to 29): 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28
+Tests failed (0 to 29): none */
